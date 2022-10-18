e389728216054422c3fbdd2824e08030b18669d6
Jerm2000 <jeremya1@byu.edu>
2022-10-17 21:31:12 -0600

    Added the order, person, and customer classes

--- hamburger.py
+++ hamburger.py
@@ -1,4 +1,25 @@
 #Description:
 #Authors:
-#   Christian Breshears
+#   Christian Breshears, Jeremy Abbott
 #Test
+from random import randrange
+
+class Order():
+  def __init__(self):
+    self.burger_count = self.randomBurgers()
+    print(self.burger_count)
+  def randomBurgers(self):
+    return randrange(1,21)
+  
+class Person():
+  def __init__(self):
+    self.customer_name = self.randomName()
+
+  def randomName(self):
+    asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
+    return asCustomers[randrange(0,10)]
+
+class Customer(Person):
+  def __init__(self):
+    super().__init__()
+    self.order = Order()
